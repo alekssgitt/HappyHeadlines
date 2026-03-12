8fdb995b95034c9341a586640bd6ea2b039459ec
alekss <anoviconoks@gmail.com>
2026-03-12 11:11:12 +0100

    Added centralized logging using seq and tracing using zipkin in Common.Shared Monitoring

---
+++ Common.Shared/Monitoring/MonitoringExtensions.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+
+namespace Common.Shared.Monitoring;
+
+public static class MonitoringExtensions
+{
+    public static WebApplicationBuilder AddSharedMonitoring(this WebApplicationBuilder builder, string serviceName)
+    {
+        var seqServerUrl = builder.Configuration["Monitoring:Seq:ServerUrl"] ?? "http://seq:5341";
+        var zipkinEndpoint = builder.Configuration["Monitoring:Zipkin:Endpoint"] ?? "http://zipkin:9411/api/v2/spans";
+
+        builder.Host.UseSerilog((context, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ServiceName", serviceName)
+                .WriteTo.Console()
+                .WriteTo.Seq(seqServerUrl);
+        });
+
+        builder.Services
+            .AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(serviceName))
+            .WithTracing(tracing =>
+            {
+                tracing
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddZipkinExporter(options => { options.Endpoint = new Uri(zipkinEndpoint); });
+            });
+
+        return builder;
+    }
+}
